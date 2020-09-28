9344235c609b6ba525ed3c2c1d58258b84999a9f
Manas Pandey <30745064+manasbolt945@users.noreply.github.com>
2020-09-28 19:05:35 +0530

    Update strongly connected components(kosaraju algo).java

--- graphs/strongly connected components(kosaraju algo).java
+++ graphs/strongly connected components(kosaraju algo).java
@@ -1,55 +1,104 @@
-class Solution {
-    //int[] ans;
-    int parent[];
-    int rank[];
-    public int[] findRedundantConnection(int[][] edges) 
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Gfg
+{
+    public static void main (String[] args)
     {
-       
-       int ans[] = new int[2];
-         parent = new int[edges.length+1];
-         rank = new int[edges.length+1];
-        for(int i=0;i<=edges.length; i++)
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
         {
-            parent[i]=i;
-        }
-        for(int key[] : edges)
-        {
-            int x = find(key[0]);
-            int y = find(key[1]);
-            if(x!=y)
-            {
-                union(x,y);
+            // arraylist of arraylist to represent graph
+            ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+            
+            int vertices = Integer.parseInt(sc.next());
+            int edges = Integer.parseInt(sc.next());
+            
+            for(int i =0; i < vertices; i++)
+                list.add(i, new ArrayList<Integer>());
                 
+            for(int i = 1; i <= edges; i++)
+            {    int u = Integer.parseInt(sc.next());
+                 int v = Integer.parseInt(sc.next());
+                 
+                 // adding directed edgese between 
+                 // vertex 'u' and 'v'
+                 list.get(u).add(v);
             }
-            else
-            {
-                ans[0]=key[0];
-                ans[1]=key[1];
-            }
-        }
-        return ans;
+            
+            Solution T = new Solution();
+            System.out.println(T.kosaraju(list, vertices));
+		}
     }
-    public int find(int x)
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+// adj : Adjacency list representing the graph
+// N: No of vertices
+class Solution
+{
+    public void DFS(ArrayList<ArrayList<Integer>> graph, int v, boolean[] visited, List<Integer> comp) 
     {
-        if(parent[x]==x)
-            return x;
-      return find(parent[x]);
+        visited[v] = true;
+        for (int i = 0; i < graph.get(v).size(); i++)
+
+            if (!visited[graph.get(v).get(i)])
+                DFS(graph, graph.get(v).get(i), visited, comp);
+
+        comp.add(v);
+    }
+    public List<Integer> fillOrder(ArrayList<ArrayList<Integer>> graph, boolean[] visited, int N) 
+    {        
+
+        List<Integer> order = new ArrayList<Integer>();
+        for (int i = 0; i < N; i++)
+
+            if (!visited[i])DFS(graph, i, visited, order);
+
+        return order;
+    }
+    public ArrayList<ArrayList<Integer>> getTranspose(ArrayList<ArrayList<Integer>> graph, int N)
+    {
+
+        ArrayList<ArrayList<Integer>> g = new ArrayList<>();
+        for (int i = 0; i < N; i++)
+            g.add(i, new ArrayList<Integer>());
+        for (int v = 0; v < N; v++)
+            for (int i = 0; i < graph.get(v).size(); i++)
+                g.get(graph.get(v).get(i)).add(v);
+
+        return g;
         
     }
-    public void union(int x, int y)
+    
+    public int kosaraju(ArrayList<ArrayList<Integer>> list, int N)
     {
-        int xrep = find(x);
-        int yrep = find(y);
-        if(rank[xrep]<rank[yrep])
-            parent[xrep]=yrep;
-        else if(rank[xrep]>rank[yrep])
-            parent[yrep]=xrep;
-        else
+        boolean[] visited = new boolean[N];
+        List<Integer> order = fillOrder(list, visited, N);       
+        ArrayList<ArrayList<Integer>> reverseGraph = getTranspose(list, N);        
+        visited = new boolean[N];
+        Collections.reverse(order);
+        List<List<Integer>> SCC = new ArrayList<>();
+        for (int i = 0; i < order.size(); i++)
         {
-            parent[yrep]=xrep;
-            rank[xrep]++;
-            
+            int v = order.get(i);
+            if (!visited[v]) 
+            {
+                List<Integer> comp = new ArrayList<>();
+                DFS(reverseGraph, v, visited, comp);
+                SCC.add(comp);
+            }
         }
+        
+        return SCC.size();
     }
-   
 }
