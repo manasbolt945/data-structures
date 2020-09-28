cc2460dcff08d3e049618cf0eef6fc999fd123f9
Manas Pandey <30745064+manasbolt945@users.noreply.github.com>
2020-09-28 20:09:01 +0530

    Create pairwise swap.java

---
+++ linked lists/pairwise swap.java
@@ -0,0 +1,88 @@
+// { Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+	
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t > 0){
+        	int n = sc.nextInt();
+            
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            
+            Solution g = new Solution();
+            head = g.pairwiseSwap(head);
+            printList(head); 
+            t--;
+        }
+    } 
+} 
+   
+// } Driver Code Ends
+
+
+
+
+/* node class of the linked list
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+*/
+
+
+class Solution {
+    // Function to pairwise swap elements of a linked list.
+    // It should returns head of the modified list
+    public Node pairwiseSwap(Node head)
+    {
+        Node alpha = head;
+        Node curr = head;
+        Node rec = head;
+        int c=0;
+        while(curr!=null && curr.next!=null)
+        {
+            int temp  =curr.data;
+            curr.data = curr.next.data;
+            curr.next.data = temp;
+            curr = curr.next.next;
+        }
+        return head;
+    }
+}
